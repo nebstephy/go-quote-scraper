af10432f6d2400dfa19686b2e122910374890a98
devopseasylearning <info@devopseasylearning.com>
2024-12-22 13:02:45 -0600

    Fix undefined variable 'quotes' in server.go

--- server.go
+++ server.go
@@ -7,23 +7,30 @@ import (
 	"time"
 )
 
-// RandomQuoteHandler serves a random quote as a JSON response.
-func RandomQuoteHandler(w http.ResponseWriter, r *http.Request) {
-	rand.Seed(time.Now().UnixNano())
-	randomIndex := rand.Intn(len(quotes))
-	response := map[string]string{
-		"quote": quotes[randomIndex],
+var quotes []string
+
+// QuotesHandler handles the /quotes endpoint
+func QuotesHandler(w http.ResponseWriter, r *http.Request) {
+	response := map[string]interface{}{
+		"quotes":       quotes,
+		"total_quotes": len(quotes),
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
 
-// QuotesHandler serves all quotes as a JSON response.
-func QuotesHandler(w http.ResponseWriter, r *http.Request) {
-	response := map[string]interface{}{
-		"total_quotes": len(quotes),
-		"quotes":       quotes,
+// RandomQuoteHandler handles the /random-quote endpoint
+func RandomQuoteHandler(w http.ResponseWriter, r *http.Request) {
+	if len(quotes) == 0 {
+		http.Error(w, "No quotes available", http.StatusInternalServerError)
+		return
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	randomIndex := rand.Intn(len(quotes))
+	response := map[string]string{
+		"quote": quotes[randomIndex],
 	}
 
 	w.Header().Set("Content-Type", "application/json")
